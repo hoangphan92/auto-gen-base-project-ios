df0e984864e9fb20e34cd62c72fb0c153217c3b4
Hoang Phan <hoangphandev@gmail.com>
2019-03-26 11:36:50 +0700

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,4 +11,4 @@ bundle exec pod install
 echo "------------ AUTO GENERATE SUCCESSFULLY ----------------"
 project_path="$(pwd)/{{ cookiecutter.project_name | replace(' ', '') }}"
 echo "- Your project path is: $project_path"
-echo "------------ CREATED AND MANTAIN BY SHAW VU -------------"
+echo "------------ CREATED AND MANTAIN BY FULL STACKOVERFLOW DEVELOPER -------------"
